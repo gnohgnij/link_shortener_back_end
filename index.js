ce10b027ab58c5095c41f56501073e79eb21672c
Chong Jing Hong <congjinghong@gmail.com>
2022-09-07 20:17:31 +0800

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const connecDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 //connect to db
 connecDB();
