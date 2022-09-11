99423b0bfbfde65ed023eb0ed1e8ea03d6a0b675
Chong Jing Hong <congjinghong@gmail.com>
2022-09-11 18:27:27 +0800

    remove cors

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const connecDB = require("./config/db");
-const cors = require("cors");
-const mysql = require("mysql");
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 //allows accepting of json data
 app.use(express.json({ extended: false }));
